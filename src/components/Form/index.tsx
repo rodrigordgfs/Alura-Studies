13e1c308f5b1a78b90dcafa7be14d237a9787789
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-03-30 22:20:41 -0300

    Adicionado sass nos componentes

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '../Button';
+import style from './Form.module.scss';
 
 class Form extends React.Component {
   render() {
     return (
-      <form>
-        <div>
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
           </label>
@@ -17,7 +18,7 @@ class Form extends React.Component {
             required
           />
         </div>
-        <div>
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">
             Tempo
           </label>
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from "react";
+import style from "./List.module.scss";
 
 function List() {
-  const tasks = [{
-    task: 'React',
-    time: '02:00:00'
-  }, {
-    task: 'Javascript',
-    time: '01:00:00'
-  }, {
-    task: "Typescript",
-    time: "03:00:00"
-  }];
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00",
+    },
+    {
+      task: "Typescript",
+      time: "03:00:00",
+    },
+  ];
   return (
-    <aside>
+    <aside className={style.listaTarefas}>
       <h2> Estudos do dia </h2>
       <ul>
         {tasks.map((item, index) => (
-          <li key={index}>
+          <li className={style.item} key={index}>
             <h3>{item.task}</h3>
             <span>{item.time}</span>
           </li>
         ))}
       </ul>
     </aside>
-  )
+  );
 }
 
 export default List;
